8a7dddecc40bbc27d7acebc8705ece7d41e9931b
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-12-28 14:41:33 +0900

    fix repository

--- internal/adaptor/infrastructure/repositoryimple/user.go
+++ internal/adaptor/infrastructure/repositoryimple/user.go
@@ -3,7 +3,7 @@ package repositoryimple
 import (
 	"context"
 	"fmt"
-	"github.com/duosonic62/go-strings-history/internal/adaptor/infrastructure/models"
+	"github.com/duosonic62/go-strings-history/internal/adaptor/infrastructure/db/models"
 	"github.com/duosonic62/go-strings-history/internal/domain/entity"
 	"github.com/duosonic62/go-strings-history/internal/domain/repository"
 	"github.com/volatiletech/sqlboiler/boil"
