860388e4e8bf95aa0879ad24b0fe784db3132e4b
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-12-30 19:34:45 +0900

    add service

--- internal/domain/repository/user.go
+++ internal/domain/repository/user.go
@@ -1,7 +1,12 @@
 package repository
 
-import "github.com/duosonic62/go-strings-history/internal/domain/entity"
+import (
+	"github.com/duosonic62/go-strings-history/internal/domain/entity"
+	"github.com/duosonic62/go-strings-history/internal/domain/valueobject"
+	"github.com/duosonic62/go-strings-history/pkg/usecase/output"
+)
 
 type UserRepository interface {
+	Find(valueobject.AuthorizationToken) (output.UserOutput, error)
 	Save(entity.User) error
 }
--- internal/usecase/inputboundary/user_query.go
+++ internal/usecase/inputboundary/user_query.go
@@ -1,7 +1,10 @@
 package inputboundary
 
-import "github.com/duosonic62/go-strings-history/internal/domain/valueobject"
+import (
+	"github.com/duosonic62/go-strings-history/internal/domain/valueobject"
+	"github.com/duosonic62/go-strings-history/pkg/usecase/input"
+)
 
 type UserQueryUseCase interface {
-	Show(token valueobject.AuthorizationToken)
+	Show(token valueobject.AuthorizationToken, ctx input.Context)
 }
