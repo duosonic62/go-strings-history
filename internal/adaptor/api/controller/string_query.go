0adece5801921215cdc6ab8149fea998ad50597a
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2020-02-02 22:15:33 +0900

    add search string controller

--- internal/adaptor/api/controller/string_query.go
+++ internal/adaptor/api/controller/string_query.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"errors"
 	"github.com/duosonic62/go-strings-history/internal/usecase/inputboundary"
 	"github.com/duosonic62/go-strings-history/pkg/usecase/input"
+	"github.com/duosonic62/go-strings-history/pkg/usecase/input/query"
+	"strconv"
 )
 
 type StringQueryController interface {
@@ -22,13 +25,45 @@ func NewStringQueryController(useCase inputboundary.StringQueryUseCase, errorUse
 }
 
 func (controller stringQueryController) GetGuitarString(ctx input.Context) {
-	id := ctx.Param("id")
-
 	authToken, err := getAuthorizationToken(ctx)
 	if err != nil {
 		controller.errorUseCase.UnauthorizedError(ctx, err)
 		return
 	}
 
+	id := ctx.Param("id")
+
 	controller.useCase.GetGuitarString(id, authToken, ctx)
+}
+
+func (controller stringQueryController) SearchGuitarString(ctx input.Context) {
+	authToken, err := getAuthorizationToken(ctx)
+	if err != nil {
+		controller.errorUseCase.UnauthorizedError(ctx, err)
+		return
+	}
+
+	queries := query.SearchGuitarString{}
+	if name, ok := ctx.GetQuery("name"); ok {
+		queries.Name = name
+	}
+	if maker, ok := ctx.GetQuery("maker"); ok {
+		queries.Maker = maker
+	}
+	if thinGauge, ok := ctx.GetQuery("thinGauge"); ok {
+		g, err := strconv.Atoi(thinGauge)
+		if err != nil {
+			controller.errorUseCase.BadRequestError(ctx, errors.New("thinGauge must be number"))
+		}
+		queries.ThickGauge = g
+	}
+	if thickGauge, ok := ctx.GetQuery("thickGauge"); ok {
+		g, err := strconv.Atoi(thickGauge)
+		if err != nil {
+			controller.errorUseCase.BadRequestError(ctx, errors.New("thickGauge must be number"))
+		}
+		queries.ThickGauge = g
+	}
+
+	controller.useCase.SearchGuitarString(queries, authToken, ctx)
 }
---
+++ pkg/usecase/input/query/search_guitar_string.go
@@ -0,0 +1,8 @@
+package query
+
+type SearchGuitarString struct {
+	Name       string
+	Maker      string
+	ThinGauge  int
+	ThickGauge int
+}
